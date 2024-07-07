d6a5e1ff9a62fc77a9de5cce2569a13843bd826a
leorr <leorr112624@gmail.com>
2024-07-07 18:57:56 -0300

    2024/07/07 18:57 Part1 courseinfo step 5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,47 +1,62 @@
 const Header = (props) => {
   return (
     <h1>
-      {props.course}
+      {props.course.name}
     </h1>
   )
 }
 
 const Part = (props) => {
   return (
-    <p>{props.partName} {props.exerciseNumber}</p>
+    <p>{props.name} {props.exercises}</p>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part partName={props.partNames[0]} exerciseNumber={props.exerciseNumbers[0]}/>
-      <Part partName={props.partNames[1]} exerciseNumber={props.exerciseNumbers[1]}/>
-      <Part partName={props.partNames[2]} exerciseNumber={props.exerciseNumbers[2]}/>
+      <Part name={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+      <Part name={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+      <Part name={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
     </div>
   )
 }
 
 const Total = (props) => {
+  let total = 0
+  props.course.parts.forEach(part => {
+    total += part.exercises
+  }
+  )
   return (
-    <p>Number of exercises {props.exerciseNumber1 + props.exerciseNumber2 + props.exerciseNumber3}</p>
+    <p>Number of exercises {total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course = {course}/>
-      <Content partNames = {[part1, part2, part3]} exerciseNumbers = {[exercises1, exercises2, exercises3]}/>
-      <Total exerciseNumber1 = {exercises1} exerciseNumber2 = {exercises2} exerciseNumber3 = {exercises3}/>
+      <Content course = {course}/>
+      <Total course = {course}/>
     </div>
   )
 }
